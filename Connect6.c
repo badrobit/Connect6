acf54b94b838bed9a8f7f8ff9a5109cbb33f03dd
Bad-Robot <mat.roscoe@unb.ca>
2012-01-13 15:32:34 -0400

    Made the game_board pretty
    Added some formatting stuff to make the game_board human legible when
    it is printed. Also added in a tracker for the last move for the ai as
    well as the skeleton for offensive movement behaviours

--- Connect6.c
+++ Connect6.c
@@ -3,21 +3,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char game_board[BOARD_SIZE][BOARD_SIZE]; 
-
+void initialize_game_board(); 
 void print_game_board(); 
 void human_place_piece(); 
+void ai_move_offensively(); 
+
+char game_board[BOARD_SIZE][BOARD_SIZE]; 
+
+int last_move_x = 0; 
+int last_move_y = 0; 
 
 int main( void )
 {
+	initialize_game_board();
+
 	while( 1 )
 	{
 		human_place_piece(); 
+		ai_move_offensively(); 
 	}
 
 	return(0); 
 }
 
+//	A function which allows a user to place a piece on the 
+// game board.
 void human_place_piece()
 {
 	int x, y = 0; 
@@ -27,20 +37,67 @@ void human_place_piece()
 	scanf( "%d", &y ); 
 
 	game_board[x-1][y-1] = 'B'; 
+
 	print_game_board(); 
 }
 
+//	A function that will capture the offensive (game winning)
+// movemnets of the AI. 
+void ai_move_offensively()
+{
+	int x, y = 0;
+	int planning = 1;  
+
+	while( planning == 1 )
+	{
+		if( last_move_x == 0 && last_move_y == 0 )
+		{
+			x = 5; y = 5; 
+		}
+
+		if( game_board[x-1][y-1] == ' ' )
+		{
+			last_move_x = x; 
+			last_move_y = y; 
+			planning == 0; 
+		}
+	}
+	game_board[x-1][y-1] = 'W';
+	print_game_board(); 
+}  
+
+// Used to print out the game board for visual display to the 
+// user. 
 void print_game_board()
+{
+	int x, y = 0;  
+
+	for( x = 0; x < (BOARD_SIZE*2)+1; x++ )
+	{
+		if( x%2 == 0 )
+			printf( "+" ); 
+		if( x%2 != 0 )
+			printf( "|" );
+		for( y = 0; y < BOARD_SIZE; y++ )
+		{
+			if( x%2 == 0 )
+				printf( "---+" ); 
+			if ( x%2 != 0 )
+				printf( " %c |", game_board[x/2][y] ); 
+		}
+		printf( "\n" ); 
+	}
+}
+
+void initialize_game_board()
 {
 	int x, y = 0;  
 
 	for( x = 0; x < BOARD_SIZE; x++ )
 	{
-		printf( "|" );
 		for( y = 0; y < BOARD_SIZE; y++ )
 		{
-			printf( " %c |", game_board[x][y] ); 
+			game_board[x][y] = ' '; 
 		}
-		printf( "\n" ); 
 	}
 }
